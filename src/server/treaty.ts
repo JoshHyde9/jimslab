2011592a668c83763c9c9b075162a94574090274
JoshHyde9 <joshhyde546@gmail.com>
2025-02-15 22:06:43 +1100

    hotfix: api url for prod and dev

--- src/server/treaty.ts
+++ src/server/treaty.ts
@@ -1,4 +1,8 @@
 import { App } from "@/app/api/[[...slugs]]/route";
 import { treaty } from "@elysiajs/eden";
 
-export const { api } = treaty<App>("localhost:3000");
+export const { api } = treaty<App>(
+  typeof window === "undefined"
+    ? `http://localhost:${process.env.PORT ?? 3000}`
+    : window.location.origin
+);
