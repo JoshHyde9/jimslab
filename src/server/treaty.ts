6f4a8f38dd85421a87f8a2d858945bf949f68a20
JoshHyde9 <joshhyde546@gmail.com>
2025-02-15 22:25:15 +1100

    hotfix: please work

--- src/server/treaty.ts
+++ src/server/treaty.ts
@@ -1,4 +1,8 @@
 import { App } from "@/app/api/[[...slugs]]/route";
 import { treaty } from "@elysiajs/eden";
 
-export const { api } = treaty<App>(process.env.AUTH_URL ?? window.location.origin);
+export const { api } = treaty<App>(
+  Bun.env.NODE_ENV === "production"
+    ? Bun.env.AUTH_URL!
+    : "http://localhost:3000"
+);
