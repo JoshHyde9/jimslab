de7b464a9941d25e35f61b76bc9dccc926139325
JoshHyde9 <joshhyde546@gmail.com>
2025-02-15 22:31:53 +1100

    hotfix: nextjs does not run bun lmfao

--- src/server/treaty.ts
+++ src/server/treaty.ts
@@ -2,7 +2,7 @@ import { App } from "@/app/api/[[...slugs]]/route";
 import { treaty } from "@elysiajs/eden";
 
 export const { api } = treaty<App>(
-  Bun.env.NODE_ENV === "production"
-    ? Bun.env.AUTH_URL!
+  process.env.NODE_ENV === "production"
+    ? process.env.AUTH_URL!
     : "http://localhost:3000"
 );
