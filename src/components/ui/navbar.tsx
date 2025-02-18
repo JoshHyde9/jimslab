b7d34b5927b6a9e76ef65c929e293b83dc47dfd4
JoshHyde9 <joshhyde546@gmail.com>
2025-02-18 11:52:33 +1100

    hotfix: mobi nav bg

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -20,7 +20,7 @@ const MobileNav = ({
 }) => {
   return (
     <div
-      className={`absolute top-0 left-0 h-screen w-screen bg-neutral-900 z-10 transform ${
+      className={`absolute top-0 left-0 h-screen w-screen bg-neutral-50 dark:bg-neutral-900 z-10 transform ${
         open ? "-translate-x-0" : "-translate-x-full"
       } transition-transform duration-300 ease-in-out filter drop-shadow-md`}
     >
